9adfa32302aedb57fdb7bfe0013a27c24de9abf2
Joe Hladnik <jhladnik20@gmail.com>
2022-09-24 19:05:03 -0500

    Add logout export to auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,13 +44,15 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout();
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+exports.logout = async (req, res) => {
+  await req.logout(() => {
+
+    req.session.destroy((err) => {
+      if (err)
+        console.log("Error : Failed to destroy the session during logout.", err);
+      req.user = null;
+      res.redirect("/");
+    });
   });
 };
 
