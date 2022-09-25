1cab5df181e551d5bfc179aa59407e9596a44bf6
Joe Hladnik <jhladnik20@gmail.com>
2022-09-24 19:29:18 -0500

    Remove multer, post createMed

--- routes/meds.js
+++ routes/meds.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middleware/multer");
 const medsController = require("../controllers/meds");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Post Routes - simplified for now
+//Med Routes - simplified for now
 router.get("/:id", ensureAuth, medsController.getList);
 
-router.post("/createMed", upload.single("file"), medsController.createMed);
+router.post("/createMed", medsController.createMed);
 
 router.put("/takeMed/:id", medsController.takeMed);
 
-router.delete("/deleteMeds/:id", medsController.deleteMed);
+router.delete("/deleteMed/:id", medsController.deleteMed);
 
 module.exports = router;
