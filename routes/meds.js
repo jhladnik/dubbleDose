4305d6111f37453ceddbf829a0914c0f799fd127
Joe Hladnik <jhladnik20@gmail.com>
2022-09-26 01:52:14 -0500

    Change med path to meds/add

--- routes/meds.js
+++ routes/meds.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const medsController = require("../controllers/meds");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Med = require("../models/Med");
 
 //Med Routes - simplified for now
-router.get("/:id", ensureAuth, medsController.getList);
+router.get("/ ", ensureAuth, medsController.getList);
 
-router.post("/createMed", medsController.createMed);
+router.post("/add", medsController.createMed);
 
-router.put("/takeMed/:id", medsController.takeMed);
+router.put("/takeMed", medsController.takeMed);
 
-router.delete("/deleteMed/:id", medsController.deleteMed);
+router.delete("/deleteMed", medsController.deleteMed);
 
 module.exports = router;
