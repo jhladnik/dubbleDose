7c20adc8773286c61de65e0a554d113c597cd675
Joe Hladnik <jhladnik20@gmail.com>
2023-02-22 20:36:49 -0600

    Add calendar-config.js to config folder

---
+++ config/calendar-config.js
@@ -0,0 +1,50 @@
+function calcTable(year){
+    let arr = new Array(12);
+    for(let x=0; x<arr.length; x++){
+        arr[x] = new Array(6);
+    };
+
+    for(let x=0; x<arr.length; x++){
+        for(let y=0; y<arr[x].length; y++){
+            arr[x][y] = new Array(7);
+        };
+    };
+
+    for(let month = 0; month<arr.length; month++){
+        
+        let startDayInWeek = new Date(year, month, 0).getDay()+1;
+
+        let monthLong = new Date(year, month+1, 0).getDate()+1;
+
+        //remove after test
+        console.log(monthLong);
+
+        let beforeCount = 0;
+        let counter = 1;
+        let startCount = false;
+
+        for(let x=0; x<arr[month].length; x++){
+            for(let y=0; y<arr[month][x].length; y++){
+
+                if(beforeCount == startDayInWeek){
+                    startCount = true;
+                }else{
+                    beforeCount++
+                }
+                
+                if(startCount == true){
+                    arr[month][x][y] == counter;
+                    counter++
+                }else{
+                    arr[month][x][y] == "";
+                }
+
+                if(counter > monthLong){
+                    arr[month][x][y] = ""
+                }
+            }
+        }
+    }
+    return arr;
+}
+module.exports = calcTable
